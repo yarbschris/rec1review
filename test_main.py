53ed9bbae72f7554e48519f781185fd060d2c63c
yarbschris <123770805+yarbschris@users.noreply.github.com>
2024-03-05 11:01:22 -0600

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,27 @@
+from main import *
+
+
+
+def test_linear_search():
+	""" done. """
+	assert linear_search([1,2,3,4,5], 5) == 4
+	assert linear_search([1,2,3,4,5], 1) == 0
+	assert linear_search([1,2,3,4,5], 6) == -1
+
+
+def test_binary_search():
+	assert binary_search([1,2,3,4,5], 5) == 4
+	assert binary_search([1,2,3,4,5], 1) == 0
+	assert binary_search([1,2,3,4,5], 6) == -1
+	### TODO: add two more tests here.
+
+	###
+
+
+def test_compare_search():
+	res = compare_search(sizes=[10, 100])
+	print(res)
+	assert res[0][0] == 10
+	assert res[1][0] == 100
+	assert res[0][1] < 1
+	assert res[1][1] < 1
